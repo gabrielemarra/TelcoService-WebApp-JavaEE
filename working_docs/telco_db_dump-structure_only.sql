84a6cd2f83d1924e4c1563e1f15e4c972f83599f
gabrielemarra <61741485+gabrielemarra@users.noreply.github.com>
2021-12-01 17:34:52 +0100

    added first version of db (DDL)

---
+++ working_docs/telco_db_dump-structure_only.sql
@@ -0,0 +1,198 @@
+CREATE DATABASE  IF NOT EXISTS `telco_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `telco_db`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: telco_db
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `employee_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `authorized` tinyint NOT NULL,
+  PRIMARY KEY (`employee_id`),
+  UNIQUE KEY `employee_id_UNIQUE` (`employee_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `optional_service`
+--
+
+DROP TABLE IF EXISTS `optional_service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `optional_service` (
+  `opt_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `price` DECIMAL(2,0) NOT NULL,
+  `quantity_sold` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`opt_id`),
+  UNIQUE KEY `opt_id_UNIQUE` (`opt_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `optional_service_available`
+--
+
+DROP TABLE IF EXISTS `optional_service_available`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `optional_service_available` (
+  `package_id` int unsigned NOT NULL,
+  `opt_id` int unsigned NOT NULL,
+  PRIMARY KEY (`package_id`,`opt_id`),
+  KEY `opt_id_av_foreign_key_idx` (`opt_id`),
+  CONSTRAINT `opt_id_av_foreign_key` FOREIGN KEY (`opt_id`) REFERENCES `optional_service` (`opt_id`),
+  CONSTRAINT `package_id_av_foreign_key` FOREIGN KEY (`package_id`) REFERENCES `service_package` (`package_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `optional_service_orderered`
+--
+
+DROP TABLE IF EXISTS `optional_service_orderered`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `optional_service_orderered` (
+  `order_id` int unsigned NOT NULL,
+  `opt_id` int unsigned NOT NULL,
+  PRIMARY KEY (`order_id`,`opt_id`),
+  KEY `opt_id_ordered_foreign_key_idx` (`opt_id`),
+  CONSTRAINT `opt_id_ordered_foreign_key` FOREIGN KEY (`opt_id`) REFERENCES `optional_service` (`opt_id`),
+  CONSTRAINT `order_id_ordered_foreign_key` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `order_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(45) NOT NULL,
+  `subscription_start` date NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `total_price` decimal(2,0) NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  `package_id` int unsigned NOT NULL,
+  PRIMARY KEY (`order_id`),
+  UNIQUE KEY `order_id_UNIQUE` (`order_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `package_id_foreign_key` FOREIGN KEY (`package_id`) REFERENCES `service_package` (`package_id`),
+  CONSTRAINT `user_id_foreign_key` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `service`
+--
+
+DROP TABLE IF EXISTS `service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service` (
+  `service_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) NOT NULL,
+  `base_price1` decimal(2,0) NOT NULL,
+  `base_price2` decimal(2,0) NOT NULL,
+  `base_price3` decimal(2,0) NOT NULL,
+  `gig_jncluded` int DEFAULT NULL,
+  `min_jncluded` int DEFAULT NULL,
+  `sms_jncluded` int DEFAULT NULL,
+  `gig_extra` int DEFAULT NULL,
+  `min_extra` int DEFAULT NULL,
+  `sms_extra` int DEFAULT NULL,
+  PRIMARY KEY (`service_id`),
+  UNIQUE KEY `service_id_UNIQUE` (`service_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `service_bundles`
+--
+
+DROP TABLE IF EXISTS `service_bundles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service_bundles` (
+  `package_id` int unsigned NOT NULL,
+  `service_id` int unsigned NOT NULL,
+  PRIMARY KEY (`package_id`,`service_id`),
+  KEY `service_id_bundle_foreign_key_idx` (`service_id`),
+  CONSTRAINT `package_id_bundle_foreign_key` FOREIGN KEY (`package_id`) REFERENCES `service_package` (`package_id`),
+  CONSTRAINT `service_id_bundle_foreign` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `service_package`
+--
+
+DROP TABLE IF EXISTS `service_package`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service_package` (
+  `package_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `validity_period` INT unsigned NOT NULL,
+  PRIMARY KEY (`package_id`),
+  UNIQUE KEY `package_id_UNIQUE` (`package_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `usercol_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-01 17:11:01
