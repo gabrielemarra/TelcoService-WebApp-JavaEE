4630778c679ebaae0d7a74416e6ab15b0daf8026
Gabriele Marra <61741485+gabrielemarra@users.noreply.github.com>
2022-02-24 01:50:31 +0100

    updated index.html and index.js

--- intellij_project/src/main/webapp/scripts/index.js
+++ intellij_project/src/main/webapp/scripts/index.js
@@ -1,38 +1,51 @@
-window.addEventListener('load', (event) => {
+$(document).ready(function () {
+    $.ajaxSetup({cache: false});
+
     $("#loginButton").click(
-        function () {
+        function (event) {
+            event.preventDefault();
             let email = $("#loginEmailInput").val();
             let password = $("#loginPasswordInput").val();
             login(email, password);
         }
-    )
+    );
+
     $("#signupButton").click(
-        function () {
+        function (event) {
+            event.preventDefault();
             let email = $("#signupEmailInput").val();
             let password = $("#signupPasswordInput").val();
             let password_confirmation = $("#signupPasswordConfirmation").val();
             signup(email, password, password_confirmation);
         }
-    )
+    );
+
+    /**
+     * This function make the AJAX call to the servlet Login and let the user logging in.
+     *
+     * @param email
+     * @param password
+     */
+    function login(email, password) {
+        let postRequest = $.post("Login", {email: email, password: password});
+
+        postRequest.done(function () {
+            alert("Login Successful");
+        });
+        postRequest.fail(function () {
+            alert("Login Error");
+        });
+        postRequest.always(function () {
+            alert("Request completed");
+        });
+
+    }
+
+    function signup(email, password, password_confirmation) {
+        // alert("Your email is: "+email+" and your password is: "+password+" - "+password_confirmation)
+    }
 });
 
-/**
- * This function make the AJAX call to the servlet Login and let the user logging in.
- *
- * @param email
- * @param password
- */
-function login(email, password) {
-    // alert("Your email is: "+email+" and your password is: "+password)
-    let postRequest = $.post("Login", {email: email, password: password})
 
-    postRequest.done(function (){
-        alert("Login successful")
-        }
-    )
 
-}
 
-function signup(email, password, password_confirmation) {
-    // alert("Your email is: "+email+" and your password is: "+password+" - "+password_confirmation)
-}
