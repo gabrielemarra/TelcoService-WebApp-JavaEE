0fef838027732bc80a04d982b5b957be21ce4d97
Gabriele Marra <61741485+gabrielemarra@users.noreply.github.com>
2022-02-24 16:16:15 +0100

    update client-side to handle JSON response

--- intellij_project/src/main/webapp/scripts/index.js
+++ intellij_project/src/main/webapp/scripts/index.js
@@ -14,9 +14,11 @@ $(document).ready(function () {
         function (event) {
             event.preventDefault();
             let email = $("#signupEmailInput").val();
+            let username = $("#signupUsernameInputInput").val();
+            let name = $("#signupNameInput").val();
             let password = $("#signupPasswordInput").val();
             let password_confirmation = $("#signupPasswordConfirmation").val();
-            signup(email, password, password_confirmation);
+            signup(email, name, username, password, password_confirmation);
         }
     );
 
@@ -29,20 +31,36 @@ $(document).ready(function () {
     function login(email, password) {
         let postRequest = $.post("Login", {email: email, password: password});
 
-        postRequest.done(function () {
-            alert("Login Successful");
+        postRequest.done(function (data, textStatus, jqXHR) {
+
+            //Here we save the data into the sessionStorage
+            let user_data = jqXHR.responseJSON;
+            sessionStorage.setItem('username', user_data.username);
+            sessionStorage.setItem('email', user_data.email);
+            sessionStorage.setItem('name', user_data.name);
+            sessionStorage.setItem('employee', user_data.employee);
+
+
+            //Redirect to the homepage (if user) or to employee page
+            window.location.href = window.location.href + jqXHR.responseJSON.new_url;
+
         });
-        postRequest.fail(function () {
+        postRequest.fail(function (jqXHR, textStatus, errorThrown) {
             alert("Login Error");
         });
-        postRequest.always(function () {
-            alert("Request completed");
-        });
+        // postRequest.always(function () {
+        //     alert("Request completed");
+        // });
 
     }
 
     function signup(email, password, password_confirmation) {
-        // alert("Your email is: "+email+" and your password is: "+password+" - "+password_confirmation)
+        if (password === password_confirmation) {
+
+            let postRequest = $.post("Signup", {email: email, password: password});
+        } else {
+            //PASSWORD DON'T MATCH!
+        }
     }
 });
 
