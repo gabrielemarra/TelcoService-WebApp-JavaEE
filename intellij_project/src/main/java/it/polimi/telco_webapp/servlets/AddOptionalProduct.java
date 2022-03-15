e937ed3b0dc2d5326c934894c8e193524148916e
dmora4 <destiny.mora@hotmail.com>
2022-03-15 22:23:31 +0100

    functionality for employees to add optional product is done! employeehomepage html only shows adding optional product with the idea to uncomment the other elements as thier repspective functinoality gets built.

--- intellij_project/src/main/java/it/polimi/telco_webapp/servlets/AddOptionalProduct.java
+++ intellij_project/src/main/java/it/polimi/telco_webapp/servlets/AddOptionalProduct.java
@@ -1,12 +1,7 @@
 package it.polimi.telco_webapp.servlets;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import it.polimi.telco_webapp.auxiliary.exceptions.InternalDBErrorException;
-import it.polimi.telco_webapp.entities.OptionalProduct;
-import it.polimi.telco_webapp.entities.User;
+
 import it.polimi.telco_webapp.services.OptionalProductService;
 import jakarta.ejb.EJB;
 import jakarta.ejb.EJBException;
@@ -19,7 +14,8 @@ import org.apache.commons.text.StringEscapeUtils;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.List;
+import java.sql.*;
+
 
 @WebServlet(name = "AddOptionalProduct", value = "/AddOptionalProduct")
 public class AddOptionalProduct extends HttpServlet {
@@ -51,19 +47,41 @@ public class AddOptionalProduct extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = StringEscapeUtils.escapeJava(request.getParameter("name"));
         String priceStr = StringEscapeUtils.escapeJava(request.getParameter("price"));
-
+/** We do not need an instantiation of an OptionalProduct object to add to the datebase...
+ * we can use use the user entries directly to populate the columns in the database
         OptionalProduct newOptionalProduct = new OptionalProduct();
         newOptionalProduct.setName(name);
         newOptionalProduct.setPrice(new BigDecimal(priceStr));
         newOptionalProduct.setQuantitySold(0);
-        newOptionalProduct.setOrders(null);
+        newOptionalProduct.setOrders(new ArrayList<Order>());
+        newOptionalProduct.setServicePackages(new ArrayList<ServicePackage>());
+ */
 
         try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://34.65.160.235:3306/telco_db", "root", "db2project2021");
+            PreparedStatement statement = con.prepareStatement("INSERT INTO optional_product VALUES (opt_id IS NULL, ?, ?, 0)");
+            /** Columns of the OptionalProduct table
+             *  Column 1: ID
+             *  Column 2: Name
+             *  Column 3: Price
+             *  Column 4: Quantity Sold
+             */
+            /**
+             *   statement.setString(1, newOptionalProduct.getName());
+             *   statement.setBigDecimal(2, newOptionalProduct.getPrice());
+             *
+             */
 
-        } catch (EJBException e) {
+            statement.setString(1, name);
+            statement.setBigDecimal(2, new BigDecimal(priceStr));
 
+            int i = statement.executeUpdate();
+            if(i > 0) {
+                //New Optional Service Successfully Added
+            }
+        } catch (EJBException | SQLException e) {
+            sendError(request, response, "InternalDBErrorException", e.getCause().getMessage());
         }
-
     }
 
     @Override
