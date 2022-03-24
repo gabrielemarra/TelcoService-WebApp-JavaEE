cf615e3b037e3ffa613a2a29519d06d52d521a54
Gabriele Marra <61741485+gabrielemarra@users.noreply.github.com>
2022-03-24 23:40:26 +0100

    display of the packages is working

--- intellij_project/src/main/webapp/scripts/buyService.js
+++ intellij_project/src/main/webapp/scripts/buyService.js
@@ -1,3 +1,43 @@
-window.addEventListener('load', (event) => {
+$(document).ready(function () {
+
+    getServicePackages()
+
+    function getServicePackages() {
+        let packages = $.get("GetAvailableServicePackages");
+        packages.done(function (data, textStatus, jqXHR) {
+            displayServicePackages(jqXHR.responseJSON);
+        });
+        packages.fail(function (jqXHR, textStatus, errorThrown) {
+            throw "UnableToRetrieveServicePackages"
+        });
+    }
+
+    function displayServicePackages(servicePackageList) {
+        let servicePackageSelect = document.getElementById("servicePackage_select");
+
+        servicePackageSelect.innerHTML="";
+
+        for (let key in servicePackageList) {
+            displayOneServicePackage(servicePackageList[key])
+        }
+
+        servicePackageSelect.classList.remove("placeholder")
+        servicePackageSelect.disabled = false;
+    }
+
+    function displayOneServicePackage(servicePackageInfo) {
+        let servicePackageSelect = document.getElementById("servicePackage_select");
+        let template = document.getElementById("package_select_row_template");
+
+        let clone = template.content.cloneNode(true);
+        let servicePackageRow = clone.querySelector("option");
+
+        servicePackageRow.textContent = servicePackageInfo.package_name;
+
+        //the package id is stored inside this custom attribute
+        servicePackageRow.dataset.package_id = servicePackageInfo.package_id
+
+        servicePackageSelect.appendChild(clone)
+    }
 
 });
