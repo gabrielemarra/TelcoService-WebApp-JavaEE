4e782b772bd73a220831b8df4533db02b9c16ca8
gabriele.marra <61741485+gabrielemarra@users.noreply.github.com>
2022-04-22 16:06:13 +0200

    Optional products placement working

--- intellij_project/src/main/java/it/polimi/telco_webapp/entities/OptionalProduct.java
+++ intellij_project/src/main/java/it/polimi/telco_webapp/entities/OptionalProduct.java
@@ -1,6 +1,7 @@
 package it.polimi.telco_webapp.entities;
 
 import jakarta.persistence.*;
+
 import java.util.List;
 import java.math.BigDecimal;
 
@@ -19,28 +20,15 @@ public class OptionalProduct {
     @Column(name = "price", nullable = false, precision = 2)
     private BigDecimal price;
 
-    // this should also be populated/ calculated by trigger
-    //@Column(name = "quantity_sold")
-    //private Integer quantitySold;
-
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "optional_product_ordered", joinColumns = @JoinColumn(name = "opt_id"),
             inverseJoinColumns = @JoinColumn(name = "order_id"))
     private List<Order> orders;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "optional_product_available", joinColumns = @JoinColumn(name = "opt_id"),
             inverseJoinColumns = @JoinColumn(name = "package_id"))
-    private List <ServicePackage> packages;
-
-
-    //public Integer getQuantitySold() {
-    //    return quantitySold;
-    //}
-
-    //public void setQuantitySold(Integer quantitySold) {
-    //    this.quantitySold = quantitySold;
-    //}
+    private List<ServicePackage> packages;
 
     public BigDecimal getPrice() {
         return price;
@@ -74,6 +62,11 @@ public class OptionalProduct {
         this.orders = orders;
     }
 
-    public void setPackages(List<ServicePackage> packages) {this.packages = packages;}
-    public List<ServicePackage> getPackages() {return packages;}
+    public void setPackages(List<ServicePackage> packages) {
+        this.packages = packages;
+    }
+
+    public List<ServicePackage> getPackages() {
+        return packages;
+    }
 }
